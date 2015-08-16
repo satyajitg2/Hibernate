71a90380ffaf649cd7ad46b2e88d83735b9e8319
satyajit singh <satyajitg2@gmail.com>
2015-08-16 21:09:14 +0530

    Hibernate example

---
+++ src/main/java/com/hibernate/App.java
@@ -0,0 +1,30 @@
+package com.hibernate;
+
+import java.util.Date;
+import org.hibernate.Session;
+import com.hibernate.util.HibernateUtil;
+import com.hibernate.user.DBUser;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("Maven + Hibernate + Oracle");
+		Session session = HibernateUtil.getSessionFactory().openSession();
+
+		session.beginTransaction();
+		DBUser user = new DBUser();
+
+		user.setUserId(100);
+		user.setUsername("superman");
+		user.setCreatedBy("system");
+		user.setCreatedDate(new Date());
+
+		session.save(user);
+		session.getTransaction().commit();
+    }
+}
